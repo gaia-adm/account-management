543438c5825687eb55b4612f8a448537648ae1ad
Richard Plotkin <richardjplotkin@gmail.com>
2016-07-17 21:49:40 -0400

    DOCKER: still not quite working

---
+++ database/init.sql
@@ -0,0 +1,10 @@
+DROP USER IF EXISTS hpe_account_management;
+CREATE USER hpe_account_management PASSWORD 'hpe_account_management' WITH SUPERUSER;
+CREATE DATABASE hpe_account_management;
+GRANT ALL PRIVILEGES ON DATABASE hpe_account_management TO hpe_account_management;
+--
+--CREATE ROLE gaia_account_management_test WITH SUPERUSER CREATEDB;
+--CREATE ROLE gaia_account_management WITH SUPERUSER CREATEDB;
+--
+--CREATE DATABASE gaia_account_management_test WITH OWNER=gaia_account_management_test;
+--CREATE DATABASE gaia_account_management WITH OWNER=gaia_account_management;
